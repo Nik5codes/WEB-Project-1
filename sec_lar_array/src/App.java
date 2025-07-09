bb71e6f85f40688b4b434cde111182b5ff15a117
Nik5codes <nikhilkunjathaya5@gmail.com>
2025-07-09 07:11:47 +0530

    Add restaurant menu, CSS demos, and Java project
    Added HTML files for a restaurant menu and tea item, CSS and HTML files demonstrating selectors and cascading, and initial Java project structure with sample code and VS Code settings. Also included an image asset used in the CSS background.

---
+++ sec_lar_array/src/App.java
@@ -0,0 +1,36 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int [] arr={5,10,2,5,9};
+        // int lar=arr[0];
+        // int slar=0;
+        // int i=0;
+        // while (i<arr.length-1) 
+        // {
+        //     if(arr[i]>lar)
+        //     {
+        //         System.out.println("arr:"+arr[i+1]+"lar:"+lar);
+        //         slar=lar;
+        //         lar=arr[i+1];
+        //     }
+        //     i++;
+        // }
+        // System.out.println("second largest is"+slar);
